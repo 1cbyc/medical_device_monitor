f5fa21d35db55bcfae7a0becc1bf6a82e94718ad
1cbyc <isaacnsisong@gmail.com>
2024-08-06 22:13:35 +0100

    another shot at building for safety critical engineering

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,11 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    with app.app_context():
-        from . import routes
-        return app
+    from . import routes
+    app.register_blueprint(routes.bp)
+    
+    return app
+    #
+    # with app.app_context():
+    #     from . import routes
+    #     return app
